a4c93fad357483d77b5e366584aa5b49f59a6551
g2coding <77990052+g2coding@users.noreply.github.com>
2021-04-25 15:21:42 +0100

    BuyTwoForOnePoundPromotion working for simple case when two products with eligible code are present

--- src/main/java/kata/supermarket/BuyTwoForOnePoundPromotion.java
+++ src/main/java/kata/supermarket/BuyTwoForOnePoundPromotion.java
@@ -1,6 +1,10 @@
 package kata.supermarket;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BuyTwoForOnePoundPromotion extends Promotion {
 
@@ -10,9 +14,15 @@ public class BuyTwoForOnePoundPromotion extends Promotion {
 
     @Override
     public List<Item> apply(List<Item> items) {
-        if (items.size() < 2) {
+        int numOfItems = items.size();
+        if (numOfItems < 2) {
             return items;
         }
-        return null;
+        BigDecimal discount = (items.get(0).price().add(items.get(1).price()).subtract(BigDecimal.ONE)).divide(new BigDecimal("2.00"), RoundingMode.HALF_UP);
+        int numOfItemsNotToBeDiscounted = numOfItems % 2;
+        int itemsToBeDiscounted = numOfItems - numOfItemsNotToBeDiscounted;
+        List<Item> itemsWithDiscounts = items.stream().limit(itemsToBeDiscounted).map(i -> i.withDiscount(discount)).collect(Collectors.toList());
+        List<Item> itemsWithoutDiscounts = items.stream().skip(itemsWithDiscounts.size()).collect(Collectors.toList());
+        return Stream.concat(itemsWithDiscounts.stream(), itemsWithoutDiscounts.stream()).collect(Collectors.toList());
     }
 }
--- src/test/java/kata/supermarket/BuyTwoForOnePoundPromotionTest.java
+++ src/test/java/kata/supermarket/BuyTwoForOnePoundPromotionTest.java
@@ -11,12 +11,25 @@ class BuyTwoForOnePoundPromotionTest {
 
     @Test
     void shouldNotProvideDiscountForOneItemBought() {
-        BuyTwoForOnePoundPromotion buyOneGetOneFreePromotion = new BuyTwoForOnePoundPromotion("A1");
+        BuyTwoForOnePoundPromotion buyTwoForOnePoundPromotion = new BuyTwoForOnePoundPromotion("A1");
         List<Item> items = List.of(new Product(new BigDecimal("0.60"), "A1").oneOf());
 
-        List<Item> itemsAfterDiscounts = buyOneGetOneFreePromotion.apply(items);
+        List<Item> itemsAfterDiscounts = buyTwoForOnePoundPromotion.apply(items);
 
         assertEquals(BigDecimal.ZERO, itemsAfterDiscounts.get(0).discount());
+    }
+
+    @Test
+    void shouldProvideDiscountForTwoItemsBought() {
+        BuyTwoForOnePoundPromotion buyTwoForOnePoundPromotion = new BuyTwoForOnePoundPromotion("A1");
+        Item crisps = new Product(new BigDecimal("0.60"), "A1").oneOf();
+        List<Item> items = List.of(crisps, crisps);
+        BigDecimal expectedDiscount = new BigDecimal("0.10");
+
+        List<Item> itemsAfterDiscounts = buyTwoForOnePoundPromotion.apply(items);
+
+        assertEquals(expectedDiscount, itemsAfterDiscounts.get(0).discount());
+        assertEquals(expectedDiscount, itemsAfterDiscounts.get(1).discount());
     }
 
 }
