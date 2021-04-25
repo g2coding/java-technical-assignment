baaf4ef810b8db9c8ae27f01b034240a8aa0ae3c
g2coding <77990052+g2coding@users.noreply.github.com>
2021-04-25 15:31:20 +0100

    BuyTwoForOnePoundPromotion working for simple case when mix of eligible and non eligible for discount products are present

--- src/main/java/kata/supermarket/BuyTwoForOnePoundPromotion.java
+++ src/main/java/kata/supermarket/BuyTwoForOnePoundPromotion.java
@@ -3,6 +3,7 @@ package kata.supermarket;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,15 +15,20 @@ public class BuyTwoForOnePoundPromotion extends Promotion {
 
     @Override
     public List<Item> apply(List<Item> items) {
-        int numOfItems = items.size();
+        Map<Boolean, List<Item>> itemsEligibleAndNonEligibleForDiscount = items.stream()
+            .collect(Collectors.partitioningBy(i -> i.getProductCode().equals(getProductCode())));
+
+        List<Item> itemsEligibleForDiscount = itemsEligibleAndNonEligibleForDiscount.get(true);
+        int numOfItems = itemsEligibleForDiscount.size();
         if (numOfItems < 2) {
             return items;
         }
-        BigDecimal discount = (items.get(0).price().add(items.get(1).price()).subtract(BigDecimal.ONE)).divide(new BigDecimal("2.00"), RoundingMode.HALF_UP);
+        BigDecimal discount = (itemsEligibleForDiscount.get(0).price().add(itemsEligibleForDiscount.get(1).price()).subtract(BigDecimal.ONE)).divide(new BigDecimal("2.00"), RoundingMode.HALF_UP);
         int numOfItemsNotToBeDiscounted = numOfItems % 2;
         int itemsToBeDiscounted = numOfItems - numOfItemsNotToBeDiscounted;
-        List<Item> itemsWithDiscounts = items.stream().limit(itemsToBeDiscounted).map(i -> i.withDiscount(discount)).collect(Collectors.toList());
-        List<Item> itemsWithoutDiscounts = items.stream().skip(itemsWithDiscounts.size()).collect(Collectors.toList());
-        return Stream.concat(itemsWithDiscounts.stream(), itemsWithoutDiscounts.stream()).collect(Collectors.toList());
+        List<Item> itemsWithDiscounts = itemsEligibleForDiscount.stream().limit(itemsToBeDiscounted).map(i -> i.withDiscount(discount)).collect(Collectors.toList());
+        List<Item> itemsWithoutDiscounts = itemsEligibleForDiscount.stream().skip(itemsWithDiscounts.size()).collect(Collectors.toList());
+        List<Item> itemsEligibleForDiscountsAfterDiscounts = Stream.concat(itemsWithDiscounts.stream(), itemsWithoutDiscounts.stream()).collect(Collectors.toList());
+        return Stream.concat(itemsEligibleForDiscountsAfterDiscounts.stream(), itemsEligibleAndNonEligibleForDiscount.get(false).stream()).collect(Collectors.toList());
     }
 }
--- src/test/java/kata/supermarket/BuyTwoForOnePoundPromotionTest.java
+++ src/test/java/kata/supermarket/BuyTwoForOnePoundPromotionTest.java
@@ -1,5 +1,6 @@
 package kata.supermarket;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -30,6 +31,24 @@ class BuyTwoForOnePoundPromotionTest {
 
         assertEquals(expectedDiscount, itemsAfterDiscounts.get(0).discount());
         assertEquals(expectedDiscount, itemsAfterDiscounts.get(1).discount());
+    }
+
+    @Test
+    @DisplayName("Should provide discount only for eligible items bought even if there are more items with different code")
+    void shouldProvideDiscountOnlyForEligibleItemsBoughtEvenIfThereAreMoreItemsWithDifferentCode() {
+        BuyTwoForOnePoundPromotion buyTwoForOnePoundPromotion = new BuyTwoForOnePoundPromotion("A1");
+        Item crisps = new Product(new BigDecimal("0.60"), "A1").oneOf();
+        Item yoghurt = new Product(new BigDecimal("0.50"), "B").oneOf();
+
+        List<Item> items = List.of(crisps, crisps, crisps, yoghurt, yoghurt);
+        BigDecimal expectedDiscount = new BigDecimal("0.10");
+
+        List<Item> itemsAfterDiscounts = buyTwoForOnePoundPromotion.apply(items);
+
+        assertEquals(expectedDiscount, itemsAfterDiscounts.get(0).discount());
+        assertEquals(expectedDiscount, itemsAfterDiscounts.get(1).discount());
+        assertEquals(BigDecimal.ZERO, itemsAfterDiscounts.get(2).discount());
+        assertEquals(BigDecimal.ZERO, itemsAfterDiscounts.get(3).discount());
     }
 
 }
